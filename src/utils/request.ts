cc24a71cce99a0be6284171922b8338b27af89ea
moyudaduizhang <2790762491@qq.com>
2024-07-12 14:07:51 +0800

    12

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const request = axios.create({
     //baseURL: 'http://39.97.218.60/',
-    baseURL: 'http://edno93937032.vicp.fun',
+    baseURL: 'https://edno93937032.vicp.fun',
     timeout: 5000
 })
 
--- src/utils/requestai.ts
+++ src/utils/requestai.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const requestai=axios.create({
-    baseURL: 'http://xp93937032.zicp.fun',
+    baseURL: 'https://xp93937032.zicp.fun',
     timeout: 5000
 })
 export default requestai
