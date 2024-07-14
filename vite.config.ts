43c9ddce5a5578bb29076804156fdefab6e35b05
moyudaduizhang <2790762491@qq.com>
2024-07-14 10:16:06 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
-
+import UonCSS from 'unocss/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
