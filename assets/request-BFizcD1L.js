3e09a0f50f239ee160978f02608cc71bb036ead2
moyudaduizhang <2790762491@qq.com>
2024-07-22 20:21:47 +0800

    1

--- assets/request-BFizcD1L.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./axios-B4uVmeYG.js";const o=e.create({baseURL:"https://edno93937032.vicp.fun",timeout:5e3});export{o as r};
---
+++ assets/requestdb-CQ0r-RDs.js
@@ -0,0 +1,1 @@
+import{a as t}from"./axios-B4uVmeYG.js";const o=t.create({baseURL:"https://5bd30447.r17.cpolar.top/multimodels",timeout:5e4});export{o as r};
