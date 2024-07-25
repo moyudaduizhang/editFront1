8e01761f58a7f0a09362882d8412003be6d13513
moyudaduizhang <2790762491@qq.com>
2024-07-25 16:31:13 +0800

    1

--- assets/requestai-6Y3IWqQb.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./axios-B4uVmeYG.js";const e=t.create({baseURL:"https://5bd30447.r17.cpolar.top/AI",timeout:5e4});export{e as r};
---
+++ assets/requestai-m6LUYzBO.js
@@ -0,0 +1,1 @@
+import{a as t}from"./axios-B4uVmeYG.js";const e=t.create({baseURL:"https://attap.cpolar.top/AI",timeout:5e4});export{e as r};
